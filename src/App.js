b8a49a00262a8adc0195c4cc0ebb65f0dc5bdf58
unknown <sb@devurai.com>
2022-04-16 16:22:35 +0300

    added form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import PostItem from "./components/PostItem";
 import {useState} from "react";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -8,9 +9,35 @@ function App() {
         {id: 2, title: 'Scala', body: 'So much description'},
         {id: 3, title: 'Ruby', body: 'There is no description'},
     ]);
+    const [post, setPost] = useState({title: '', body: ''});
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+
+        setPosts([...posts, {...post, id: Date.now()}])
+        setPost({title: '', body: ''});
+    }
+
   return (
     <div className="App">
-        <PostList posts={posts} title={"List 1"}/>
+        <form>
+            {/*Controled Input Управляемый*/}
+            <MyInput
+                value={post.title}
+                onChange={e => setPost( {...post, title: e.target.value})}
+                type="text"
+                placeholder="Post title"
+            />
+
+            {/*useRef forwardRef неУправляемый*/}
+            <MyInput
+                value={post.body}
+                onChange={e => setPost( {...post, body: e.target.value})}
+                type="text"
+                placeholder="Post description"
+            />
+            <MyButton onClick={addNewPost}>Create post</MyButton>
+        </form>
         <PostList posts={posts} title={"List 1"}/>
     </div>
   );
