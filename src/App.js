991ea4c61b74ce1087f927e7e46da7e89d067bb5
unknown <sb@devurai.com>
2022-04-16 15:16:42 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import PostItem from "./components/PostItem";
+import {useState} from "react";
+import PostList from "./components/PostList";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Scala', body: 'So much description'},
+        {id: 3, title: 'Ruby', body: 'There is no description'},
+    ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PostList posts={posts} title={"List 1"}/>
+        <PostList posts={posts} title={"List 1"}/>
     </div>
   );
 }
